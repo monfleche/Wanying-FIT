3ebd04056c1ccf99543ba2d0b8cc939eb21b6bf2
Wanying Zhao <wanying.zhao@outlook.com>
2017-02-23 08:22:11 +0100

    beginning

--- Python Project1.py
+++ Python Project1.py
@@ -1,3 +1,24 @@
-print("Hello world")
+################################################################################################
+##
+##  Python Project
+##  Master 2 FIT
+##  Wanying ZHAO & Amadou DJIBO
+##
+#################################################################################################
 
-print(3+3)
+
+
+
+##############Create Bonds#############
+
+class Bonds(object):
+    def __init__(self, minimum_term, minimum_amount, yearly_interest_rate)
+        self.minimum_term=minimum_term
+        self.minimum_amount=minimum_amount
+        self.yearly_interest_rate=yearly_interest_rate
+
+Short_term_bonds=Bonds("2 years","1000","1%")
+Long_term_bonds=Bonds("5 years", "3000", "5%")
+
+
+
